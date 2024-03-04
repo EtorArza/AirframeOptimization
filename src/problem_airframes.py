1b46ead0f6b5211f611e0f91d81ca06ff9908c5c
Etor Arza <etorarza@gmail.com>
2024-03-04 16:31:20 +0100

    interactive plot airframes problem

--- src/problem_airframes.py
+++ src/problem_airframes.py
@@ -34,22 +34,24 @@ def plot_airframe_design(x: numpy.typing.ArrayLike):
     e2 = np.array([0,1,0])
     e3 = np.array([0,0,1])
 
-    frame_scale = 0.26
+    frame_scale_plane = 0.16
+    frame_scale_normal = 0.26
+
     for i in range(len(pars.motor_orientations)):
         t_vec = pars.motor_translations[i]
         R = Rotation.from_euler("xyz", pars.motor_orientations[i], degrees=True).as_matrix()
         if pars.motor_directions[i] == -1:
-            color = "m"
+            color = "blue"
         elif pars.motor_directions[i] == 1:
-            color = "r"
+            color = "orange"
         else:
             raise ValueError("Direction should be either 1 or -1. Instead, pars.motor_directions[i]=", pars.motor_directions[i])
         ax.add_line(Line3D([0,t_vec[0]],[0,t_vec[1]],[0,t_vec[2]]))
-        ax.add_line(Line3D([t_vec[0],t_vec[0]+R[0,:]@e1*frame_scale],[t_vec[1],t_vec[1]+R[1,:]@e1*frame_scale],[t_vec[2],t_vec[2]+R[2,:]@e1*frame_scale],color=color)) #x
-        ax.add_line(Line3D([t_vec[0],t_vec[0]+R[0,:]@e2*frame_scale],[t_vec[1],t_vec[1]+R[1,:]@e2*frame_scale],[t_vec[2],t_vec[2]+R[2,:]@e2*frame_scale],color=color)) #y
-        ax.add_line(Line3D([t_vec[0],t_vec[0]-R[0,:]@e1*frame_scale],[t_vec[1],t_vec[1]-R[1,:]@e1*frame_scale],[t_vec[2],t_vec[2]-R[2,:]@e1*frame_scale],color=color)) #x
-        ax.add_line(Line3D([t_vec[0],t_vec[0]-R[0,:]@e2*frame_scale],[t_vec[1],t_vec[1]-R[1,:]@e2*frame_scale],[t_vec[2],t_vec[2]-R[2,:]@e2*frame_scale],color=color)) #y
-        ax.add_line(Line3D([t_vec[0],t_vec[0]+R[0,:]@e3*frame_scale],[t_vec[1],t_vec[1]+R[1,:]@e3*frame_scale],[t_vec[2],t_vec[2]+R[2,:]@e3*frame_scale],color='g')) #z
+        ax.add_line(Line3D([t_vec[0],t_vec[0]+R[0,:]@e1*frame_scale_plane],[t_vec[1],t_vec[1]+R[1,:]@e1*frame_scale_plane],[t_vec[2],t_vec[2]+R[2,:]@e1*frame_scale_plane],color=color)) #x
+        ax.add_line(Line3D([t_vec[0],t_vec[0]+R[0,:]@e2*frame_scale_plane],[t_vec[1],t_vec[1]+R[1,:]@e2*frame_scale_plane],[t_vec[2],t_vec[2]+R[2,:]@e2*frame_scale_plane],color=color)) #y
+        ax.add_line(Line3D([t_vec[0],t_vec[0]-R[0,:]@e1*frame_scale_plane],[t_vec[1],t_vec[1]-R[1,:]@e1*frame_scale_plane],[t_vec[2],t_vec[2]-R[2,:]@e1*frame_scale_plane],color=color)) #x
+        ax.add_line(Line3D([t_vec[0],t_vec[0]-R[0,:]@e2*frame_scale_plane],[t_vec[1],t_vec[1]-R[1,:]@e2*frame_scale_plane],[t_vec[2],t_vec[2]-R[2,:]@e2*frame_scale_plane],color=color)) #y
+        ax.add_line(Line3D([t_vec[0],t_vec[0]+R[0,:]@e3*frame_scale_normal],[t_vec[1],t_vec[1]+R[1,:]@e3*frame_scale_normal],[t_vec[2],t_vec[2]+R[2,:]@e3*frame_scale_normal],color='g')) #z
 
     plt.show()
 
@@ -60,6 +62,7 @@ def from_0_1_to_airframe(x: numpy.typing.ArrayLike):
         return [el for el in [row for row in m]]
 
     # A linear scalling to [0,1]^number_of_parameters
+    assert type(x)==np.ndarray, "x = "+str(x)+" | type(x) = "+str(type(x))
     l = len(x)
     assert l%6 == 0
     n_rotors = l // 6
@@ -94,37 +97,130 @@ def constraint_check(x: numpy.typing.ArrayLike):
     check1, check2 = analyze_robot_config.analyze_robot_config(robot)
     return (check1, check2)
 
+
+
+def _plot_airframe_design(ax, params):
+
+    og_pars = np.array([el for el in params.values()])
+    print(og_pars)
+    pars = from_0_1_to_airframe(og_pars)
+
+    assert len(pars.motor_orientations) == len(pars.motor_translations)
+
+    xlim = ylim = zlim = [-1.2,1.2]
+    ax.set_xlim(xlim); ax.set_ylim(ylim); ax.set_zlim(zlim)
+
+    ax.set_xlabel('x',size=18)
+    ax.set_ylabel('y',size=18)
+    ax.set_zlabel('z',size=18)
+    
+    e1 = np.array([1,0,0])
+    e2 = np.array([0,1,0])
+    e3 = np.array([0,0,1])
+
+    frame_scale_plane = 0.16
+    frame_scale_normal = 0.26
+
+    for i in range(len(pars.motor_orientations)):
+        t_vec = pars.motor_translations[i]
+        R = Rotation.from_euler("xyz", pars.motor_orientations[i], degrees=True).as_matrix()
+        if pars.motor_directions[i] == -1:
+            color = "blue"
+        elif pars.motor_directions[i] == 1:
+            color = "orange"
+        else:
+            raise ValueError("Direction should be either 1 or -1. Instead, pars.motor_directions[i]=", pars.motor_directions[i])
+        ax.add_line(Line3D([0,t_vec[0]],[0,t_vec[1]],[0,t_vec[2]]))
+        ax.add_line(Line3D([t_vec[0],t_vec[0]+R[0,:]@e1*frame_scale_plane],[t_vec[1],t_vec[1]+R[1,:]@e1*frame_scale_plane],[t_vec[2],t_vec[2]+R[2,:]@e1*frame_scale_plane],color=color)) #x
+        ax.add_line(Line3D([t_vec[0],t_vec[0]+R[0,:]@e2*frame_scale_plane],[t_vec[1],t_vec[1]+R[1,:]@e2*frame_scale_plane],[t_vec[2],t_vec[2]+R[2,:]@e2*frame_scale_plane],color=color)) #y
+        ax.add_line(Line3D([t_vec[0],t_vec[0]-R[0,:]@e1*frame_scale_plane],[t_vec[1],t_vec[1]-R[1,:]@e1*frame_scale_plane],[t_vec[2],t_vec[2]-R[2,:]@e1*frame_scale_plane],color=color)) #x
+        ax.add_line(Line3D([t_vec[0],t_vec[0]-R[0,:]@e2*frame_scale_plane],[t_vec[1],t_vec[1]-R[1,:]@e2*frame_scale_plane],[t_vec[2],t_vec[2]-R[2,:]@e2*frame_scale_plane],color=color)) #y
+        ax.add_line(Line3D([t_vec[0],t_vec[0]+R[0,:]@e3*frame_scale_normal],[t_vec[1],t_vec[1]+R[1,:]@e3*frame_scale_normal],[t_vec[2],t_vec[2]+R[2,:]@e3*frame_scale_normal],color='g')) #z
+
+
+
+
+
+
+def plot_airframe_interactive():
+    import matplotlib.pyplot as plt
+    from matplotlib.widgets import Button, Slider
+
+    params={
+    'r1':0.5,
+    'r2':0.1,
+    'r3':0.1,
+    'theta1':0.2,
+    'theta2':0.1,
+    'theta3':0.3,
+    }
+
+    # Create the figure and the line that we will manipulate
+    fig = plt.figure()
+    xlim = ylim = zlim = [-1.2,1.2]
+    ax = fig.add_subplot(projection='3d', xlim=xlim, ylim=ylim, zlim=zlim)
+
+    _plot_airframe_design(ax, params)
+
+    # adjust the main plot to make room for the sliders
+    fig.subplots_adjust(left=0.25, bottom=0.55)
+
+
+
+    i = -1
+    axes_list = []
+    slider_list = []
+    update_list = []
+
+
+    def make_update(i):
+        def update(val):
+            key = list(params.keys())[i]
+            print("update ", i)
+            ax.clear()
+            params[key] = val
+            print("updated ", (key, val))
+            _plot_airframe_design(ax, params)
+            fig.canvas.draw_idle()
+        return update
+
+
+    for key, item in params.items():
+        i += 1
+        axes_list.append(fig.add_axes([0.25, 0.5*i/9, 0.65, 0.03]))
+        slider_list.append(Slider(
+            ax=axes_list[i],
+            label=key,
+            valmin=0.0,
+            valmax=1.0,
+            valinit=item,
+        ))
+
+
+        update_list.append(make_update(i))
+        slider_list[-1].on_changed(update_list[-1])
+
+
+    plt.show()
+
+
+
 if __name__ == "__main__":
 
 
-    pars = from_0_1_to_airframe(np.array([
+    plot_airframe_interactive()
+    exit(0)
+
+    og_pars = np.array([
     0,0,0,
     0.25,0,0,
-    0.25,0.25,0,
-    0.25,0.25,0.25,
-
-    0,0,0,
-    0.25,0,0,
-    0.25,0.25,0,
-    0.25,0.25,0.25,
-    ]))
-
-
-    n_rotors = 4
-    n_montecarlo = 2000
-    success = 0
-    for i in range(n_montecarlo):
-        pars = from_0_1_to_airframe(np.random.random(6*n_rotors))
-        robot = RobotModel(pars)
-        check1, check2 = analyze_robot_config.analyze_robot_config(robot)
-        success += int(check1 and check2)
+    ])
 
 
 
-    print("Proportion of acceptable solutions:", success / n_montecarlo)
 
 
-    # plot_airframe_design(pars)
+    plot_airframe_design(og_pars)
 
 
     # # If I change these parameters, is that the full set of possible quad configurations?
