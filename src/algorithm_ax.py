7db4089a2872958d85450904180941e69bb35cc0
Etor Arza <etorarza@gmail.com>
2024-06-06 14:47:20 +0200

    make ax reproducible

--- src/algorithm_ax.py
+++ src/algorithm_ax.py
@@ -3,9 +3,33 @@ import threading
 import queue
 from interfaces import *
 from ax import optimize
+from contextlib import contextmanager
+import random
+import numpy as np
+import torch
+
+
+
+
+def restore_global_rs(old_state: dict):
+    random.setstate(old_state["random"])
+    np.random.set_state(old_state["numpy"])
+    torch.set_rng_state(old_state["torch"])
+
+
+def set_global_rs(seed: int) -> dict:
+    old_state = {
+        "random": random.getstate(),
+        "numpy": np.random.get_state(),
+        "torch": torch.get_rng_state(),
+    }
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    return old_state
+
 
 # https://ax.dev/docs/bayesopt.html
-
 class ax_optimizer:
 
     def __init__(self, prob:problem, seed: int, total_budget: int):
@@ -15,11 +39,12 @@ class ax_optimizer:
 
 
         def fun(x):
-
             # numpy array for x from wierd framework format
+            restore_global_rs(self.old_state)
             self.x_queue.put(np.array([el[1] for el in sorted(x.items(), key=lambda z: float(z[0].strip("x")))]))
             f_res = self.f_queue.get(timeout=30000.0)
             assert type(f_res)==float or type(f_res) == np.float64, "f_res = "+str(f_res) + "| type = " + str(type(f_res))
+            set_global_rs(self.rs.randint(int(4294967293)))
             return f_res
 
         self.x_queue = queue.Queue()
@@ -39,7 +64,7 @@ class ax_optimizer:
                 minimize=True,
                 total_trials=total_budget
             )
-
+        self.old_state = set_global_rs(self.rs.randint(4294967293))
         thread = threading.Thread(target=minimize, args=[], daemon=True)
         thread.start()
 
